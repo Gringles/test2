f3da753651c95ad0de9fdcad8f91f00b9be29db8
Gringles <42545430+Gringles@users.noreply.github.com>
2018-08-21 00:25:01 +0100

    zz

--- Init.sh
+++ Init.sh
@@ -13,6 +13,7 @@ sudo systemctl enable docker
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
 read -n1 -r -p "Press anything to continue"
