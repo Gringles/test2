4a4f1a6dc93b85a5d7ac9672d752b2b3ea4b4a6a
Gringles <42545430+Gringles@users.noreply.github.com>
2018-08-21 02:54:22 +0100

    z

--- Init.sh
+++ Init.sh
@@ -2,7 +2,8 @@ sudo mkdir /Software
 cd /Software
 echo 
 read -n1 -r -p "Install Docker?" input
-if ["$input" -eq 'y'] then
+if ["$input" = 'y']
+then
 	sudo yum install wget
 	sudo wget https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-18.03.1.ce-1.el7.centos.x86_64.rpm
 	sudo yum install docker-ce-18.03.1.ce-1.el7.centos.x86_64.rpm
